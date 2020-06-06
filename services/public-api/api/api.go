6d2ef3dc9463573439e98fcc3f4047ccf135ba3f
Steven Elleman <selleman@berkeley.edu>
2020-06-06 16:19:25 -0700

    Use cors AllowOrigin to allow exception to same-origin policy

--- services/public-api/api/api.go
+++ services/public-api/api/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"sg/services/public-api/db"
@@ -14,14 +15,20 @@ func main() {
 	// TODO: need authz middleware to convert cookie into FI list
 	router := gin.Default()
 
+	// TODO: Pass in as argument
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+	}))
+
 	// TODO: Also pass in some kind of response writer object
 	// Let the API endpoints begin...
 	v1 := router.Group("/v1")
-	v1.GET("/silos", handlers.ListSilos) // Works
-	v1.GET("/silos/:silo_id", handlers.GetSilo) // Works
-	v1.POST("/silos/:silo_id", handlers.CreateSilo) // Works
+	v1.GET("/silos", handlers.ListSilos)
+	v1.GET("/silos/:silo_id", handlers.GetSilo)
+	v1.POST("/silos/:silo_id", handlers.CreateSilo)
 	v1.PUT("/silos/:silo_id", handlers.UpdateSilo)
-	v1.DELETE("/silos/:silo_id", handlers.DeleteSilo) // Works
+	v1.DELETE("/silos/:silo_id", handlers.DeleteSilo)
 
+	// TODO: Pass in as argument
 	router.Run("localhost:8000")
 }
