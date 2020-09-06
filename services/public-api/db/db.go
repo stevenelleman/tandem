5c36ad9e7ee0d9a208330182d3b11a0fbb66b40d
Steven Elleman <selleman@berkeley.edu>
2020-09-05 23:29:04 -0700

    DB layer changes

--- services/public-api/db/db.go
+++ services/public-api/db/db.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"libraries/datastore/migrater"
 
 	_ "github.com/lib/pq"
 )
@@ -15,32 +14,24 @@ const (
 	name     = "postgres"
 )
 
-// TODO: currently is a global variable. Connection to db should be instantiated on a per-request basis.
-var DB *sql.DB
-
-func InitDb(host string) {
-	var err error
+func InitDb(host string, conns int) *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port,
 		user, password, name)
 
-	DB, err = sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	err = DB.Ping()
+
+	// TODO: tune
+	db.SetMaxOpenConns(conns)
+
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Public-API Datastore: Successfully connected!")
-
-	// Apply database migrations on Public-API Datastore
-	m := migrater.Migrater{}
-	count, err := m.Up(DB)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Public-API Datastore: Executed %d migrations\n", count)
+	return db
 }
