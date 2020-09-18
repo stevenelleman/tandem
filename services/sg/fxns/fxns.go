e277d60f6cfc2c84c965030da40652030236fa34
Steven Elleman <selleman@berkeley.edu>
2020-09-17 22:04:23 -0700

    Add functions with inverse arguments

--- services/sg/fxns/fxns.go
+++ services/sg/fxns/fxns.go
@@ -15,14 +15,34 @@ func Replace(d, s string, start, end int) string {
 	return d[0:start] + s + d[end:]
 }
 
+// TODO: Add affected region in both directions, that way the
+// 	rest of the doc doesn't need to be transformed
+// How to have multiple affected regions?
+// Replace plus inverse arguments
+func ReplaceInverse(d, s string, start, end int) (string, string, int, int) {
+	removed := d[start:end]
+	inverseStart := start
+	inverseEnd := start + len(s)
+	return d[0:start] + s + d[end:], removed, inverseStart, inverseEnd
+}
+
 // Return new doc with embedded source
 // A: Start is inclusive
 func Transclude(d, s string, start int) string {
 	return d[0:start] + s + d[start:]
 }
 
+// Cannot capture inverse because there's no deletion
+func TranscludeInverse(d, s string, start, end int) (string, string, int, int) {
+	inverseStart := start
+	inverseEnd := start + len(s)
+	return d[0:start] + s + d[end:], "", inverseStart, inverseEnd
+}
+
 // Return new source that's attached to the doc, the highlight is _part_ of the doc
 // A: Start and end are inclusive
+// HERE: How to represent inverse? Matter of turning some kind of source characteristic back
+// alternatively could make an "unmake" source, which "unasks" the question
 func Highlight(doc0 string, start, end int) string {
 	return doc0[start:end]
 }
@@ -33,6 +53,19 @@ func Drag(d, s string, start0, start1 int) string {
 	d1 := d[0:start0] + d[start0+len(s):]
 	return d1[0:start1] + s + d1[start1:]
 }
+
+func DragInverse(d string, start0, end0, start1 int) (string, int, int, int) {
+	drag := d[start0:end0]
+
+	start0Inverse := start1
+	end0Inverse := start1 + len(drag)
+	start1Inverse := start0
+	d1 := d[0:start0] + d[end0:]
+	return d1[0:start1] + drag + d1[start1:], start0Inverse, end0Inverse, start1Inverse
+}
+
+// Mark the surrounding context for some highlight
+func Context() {}
 
 // Start a new source, references back to the document
 // What would this look like?
--- services/sg/fxns/fxns_test.go
+++ services/sg/fxns/fxns_test.go
@@ -41,3 +41,71 @@ func TestBasics(t *testing.T) {
 	doc5 := Highlight(doc4, 6, 10)
 	require.Equal(t, "blah", doc5)
 }
+
+func TestInverse(t *testing.T) {
+	src1 := "hello..."
+
+	// Replace -- Should be inverse
+	// Addition
+	doc1, isrc, istart, iend := ReplaceInverse("", src1, 0, 0)
+	require.Equal(t, src1, doc1)
+
+	idoc1, src, start, end := ReplaceInverse(doc1, isrc, istart, iend)
+	require.Equal(t, "", idoc1)
+	require.Equal(t, src1, src)
+	require.Equal(t, 0, start)
+	require.Equal(t, 0, end)
+
+	// Deletion
+	doc2, isrc, istart, iend := ReplaceInverse(doc1, "", 0, 5)
+	require.Equal(t, "...", doc2)
+
+	idoc2, src, start, end := ReplaceInverse(doc2, isrc, istart, iend)
+	require.Equal(t, doc1, idoc2) // HERE: Failing
+	require.Equal(t, "", src)
+	require.Equal(t, 0, start)
+	require.Equal(t, 5, end)
+
+	// Deletion
+	doc2, isrc, istart, iend = ReplaceInverse(doc1, "", 5, 8)
+	require.Equal(t, "hello", doc2)
+
+	idoc2, src, start, end = ReplaceInverse(doc2, isrc, istart, iend)
+	require.Equal(t, doc1, idoc2)
+	require.Equal(t, "", src)
+	require.Equal(t, 5, start)
+	require.Equal(t, 8, end)
+
+	// Replace
+	doc2, isrc, istart, iend = ReplaceInverse(doc1, " hello", 5, 8)
+	require.Equal(t, "hello hello", doc2)
+
+	idoc2, src, start, end = ReplaceInverse(doc2, isrc, istart, iend) // here
+	require.Equal(t, doc1, idoc2)
+	require.Equal(t, " hello", src)
+	require.Equal(t, 5, start)
+	require.Equal(t, 8, end)
+
+	// Transclude -- Is not inverse but still returns initial state
+	doc3, isrc, istart, iend := TranscludeInverse(doc2, " bye", 5, 5)
+	require.Equal(t, "hello bye hello", doc3)
+
+	idoc3, src, start, end := TranscludeInverse(doc3, isrc, istart, iend)
+	require.Equal(t, doc2, idoc3)
+
+	doc4, isrc, istart, iend := TranscludeInverse(doc3, " blah", 9, 9)
+	require.Equal(t, "hello bye blah hello", doc4)
+
+	idoc4, _, _, _ := ReplaceInverse(doc4, isrc, istart, iend)
+	require.Equal(t, doc3, idoc4)
+
+	// Drag -- Seems to be inverse -- need to figure out logic of what is invertible
+	doc5, istart0, iend0, istart1 := DragInverse(doc4, 6, 10, 11)
+	require.Equal(t, "hello blah bye hello", doc5)
+
+	idoc4, start0, end0, start1 := DragInverse(doc5, istart0, iend0, istart1)
+	require.Equal(t, doc4, idoc4)
+	require.Equal(t, 6, start0)
+	require.Equal(t, 10, end0)
+	require.Equal(t, 11, start1)
+}
