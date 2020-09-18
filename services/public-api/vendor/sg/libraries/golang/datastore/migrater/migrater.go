294681b0684668f3c9cecc20f902039f6c744477
Steven Elleman <selleman@berkeley.edu>
2020-09-18 12:43:30 -0700

    Vendor changes

--- services/public-api/vendor/sg/libraries/golang/datastore/migrater/migrater.go
+++ services/public-api/vendor/sg/libraries/golang/datastore/migrater/migrater.go
@@ -7,27 +7,48 @@ import (
 	migrate "github.com/rubenv/sql-migrate"
 )
 
-// TODO: Add to constants
-const pql = "postgres"
-const tableName = "public-api"
-const migrationPath = "./migrations"
+type Args struct {
+	Dialect   string
+	TableName string
+	Path      string
+}
 
-type Migrater struct{}
+func MakeArgs(dialect, tableName, path string) *Args {
+	return &Args{
+		Dialect:   dialect,
+		TableName: tableName,
+		Path:      path,
+	}
+}
+
+type Migrater struct {
+	dialect   string
+	tableName string
+	path      string // path to migrations
+}
+
+func MakeMigrater(args *Args) *Migrater {
+	return &Migrater{
+		dialect:   args.Dialect,
+		tableName: args.TableName,
+		path:      args.Path,
+	}
+}
 
 func (m *Migrater) SetTable() {
-	migrate.SetTable(tableName)
+	migrate.SetTable(m.tableName)
 }
 
 func (m *Migrater) Source() *migrate.PackrMigrationSource {
 	return &migrate.PackrMigrationSource{
-		Box: packr.New(tableName, migrationPath),
+		Box: packr.New(m.tableName, m.path),
 	}
 }
 
 func (m *Migrater) Up(db *sql.DB) (int, error) {
-	return migrate.Exec(db, pql, m.Source(), migrate.Up)
+	return migrate.Exec(db, m.dialect, m.Source(), migrate.Up)
 }
 
 func (m *Migrater) Down(db *sql.DB) (int, error) {
-	return migrate.Exec(db, pql, m.Source(), migrate.Down)
+	return migrate.Exec(db, m.dialect, m.Source(), migrate.Down)
 }
