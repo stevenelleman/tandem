90064d07c70a2f34fa054c46b1f550e7c86a0816
Steven Elleman <selleman@berkeley.edu>
2021-04-06 16:38:11 -0700

    Remove sg- prefix

--- services/public-api/constants/constants.go
+++ services/public-api/constants/constants.go
@@ -1,12 +1,14 @@
 package constants
 
+// TODO: How to more tightly couple these values to k8s.yaml? It would be idea to have one
+
 const (
 	MaxConns                  = 40
 	PublicAPIPort             = 8000
 	LocalWebFrontendHost      = "http://localhost:3000"
 	DockerComposeHost         = "http://localhost"
 	TiltHost                  = "http://localhost:8000"
-	SGServiceAddress          = "sg-sg:8001"
+	SGServiceAddress          = "grpc:8001"
 	APIStoreDriver            = "postgres"
 	APIStorePort              = 5432
 	APIStoreUser              = "postgres-user"
