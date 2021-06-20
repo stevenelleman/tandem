a3355570c638f0b93b7460b1c3f68eaf9fd8f52f
Steven Elleman <selleman@berkeley.edu>
2021-06-20 09:07:33 -0700

    Add cmd directory to go services

---
+++ services/public-api/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+	"web.microservice.shell/services/public-api/api"
+)
+
+func main() {
+	host := os.Args[1]
+	if host == "" {
+		panic("Host not defined")
+	}
+	store := os.Args[2]
+	if store == "" {
+		panic("Store not defined")
+	}
+	api.Run(store)
+}
