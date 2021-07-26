be8b069d3d2756b236320fa6f2219490b86317e2
Nathan Mandi <nathanmandi@Nathans-MacBook-Pro-2.local>
2021-07-25 21:56:43 -0700

    Methods interface in place

--- services/public-api/internal/graph/methods.go
+++ services/public-api/internal/graph/methods.go
@@ -1,8 +1,43 @@
-package transformations
+package graph
 
 import (
 	"math/rand"
 )
+
+// ----------------------------------------------------
+// Methods for user functionality
+
+// Create the first node in a document, with no source edges
+func CreateNewDocument() (*Node) {
+	node := &Node {
+		Id: GenerateUUID(),
+		CreatedAt: time.Now(), // TODO: Express as a PreInsert so db clock is source of truth
+	}
+	// Create a new scope for the new document
+	scope := &Scope {
+		ScopeId: GenerateUUID(),
+		Nodes: [node.NodeId]
+	}
+	node.scopes = append(node.scopes, scope)
+	return node
+}
+
+// Take in some source node, create an edge and a target node
+func Transform(parent *models.Node, args *models.FxnArgs) (*models.Node, *models.Edge) {
+	targetDoc, iargs := Call(parent.Document, args)
+	child := models.ConstructNode(targetDoc, parent)
+	edge := models.ConstructEdge(args, iargs, parent.Id, child.Id)
+	child.EdgeIn = edge.Id
+	return child, edge
+}
+
+// Create a new scope from an existing node
+func ForkDocument() () {
+	// TODO
+}
+
+// ----------------------------------------------------
+// Internal Methods
 
 func GenerateUUID() (id int) {
 	return utils.UUIDv4().String()
@@ -36,46 +71,21 @@ func ConstructScope(node *Node) (*Node) {
 	// TODO
 }
 
-// Create the first node in a document, with no source edges
-func CreateNewDocument() (*Node) {
-	node := &Node {
-		Id: GenerateUUID(),
-		CreatedAt: time.Now(), // TODO: Express as a PreInsert so db clock is source of truth
-	}
 
-	// Create a new scope for the new document
-	scope := &Scope {
-		ScopeId: GenerateUUID(),
-		Nodes: [node.NodeId]
-	}
-
-	node.scopes = append(node.scopes, scope)
-	return node
-}
-
-// Take in some source node, create an edge and a target node
-func Transform(parent *models.Node, args *models.FxnArgs) (*models.Node, *models.Edge) {
-	targetDoc, iargs := Call(parent.Document, args)
-	child := models.ConstructNode(targetDoc, parent)
-	edge := models.ConstructEdge(args, iargs, parent.Id, child.Id)
-	child.EdgeIn = edge.Id
-	return child, edge
-}
-
-func ExtractDocFromTransformation(edge Edge*) (doc string) {
+func AccumulateDocForEdge(edge Edge*) (doc string) {
 	var region string := "" // Do we need this?
 	var args Args* := edge.Args
 	var sourceNode Node* = GetNodeById(graph, edge.SourceNodeId) // TODO set up preprocessing to access graph as go object, cache as necessary
-	var sourceDoc string := AssembleDocForNode(sourceNode)
+	var sourceDoc string := AccumulateDocForNode(sourceNode)
 
 	targetDoc, inverseDoc, invArgs := Call(region, sourceDoc, args)
 	return targetDoc
 }
-func AssembleDocForNode(node Node*) (doc string) {
+func AccumulateDocForNode(node Node*) (doc string) {
 	if (node.DocIsPopulated) {
 		return node.CheckpointDoc
 	}
 
-	// Extract doc from source edge. Recursively accumulate it.
-	return ExtractDocFromTransformation(currentNode.EdgesIn[0]) // Currently assumes one parent! Will be a challenge to deal with multiple
+	// Accumulate doc from source edge. Recursively accumulate it.
+	return AccumulateDocForEdge(currentNode.EdgesIn[0]) // Currently assumes one parent! Will be a challenge to deal with multiple
 }
