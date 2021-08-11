0643abbe1860b1372025f0adc10740fc1a95ffea
Jack Rosenberg <jackrosenberg99@gmail.com>
2021-08-11 16:36:20 -0700

    Intermediate Footer + random extra

--- services/web-frontend/internal/src/ui/containers/Footer.tsx
+++ services/web-frontend/internal/src/ui/containers/Footer.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 export class Footer extends React.Component {
   render() {
     return (
-      <div className="footer-container" />
+      <div className="footer-container">
+        <div className="footer-links">
+          <div className="footer-link">
+            <a href="">Link 1</a>
+          </div>
+          <div className="footer-link">
+            <a href="">Link 2</a>
+          </div>
+          <div className="footer-link">
+            <a href="">Link 3</a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
