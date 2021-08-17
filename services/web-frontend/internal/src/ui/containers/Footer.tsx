5f3c53754b5136b766c0c55cff29e3d6da78a052
Jack Rosenberg <jackrosenberg99@gmail.com>
2021-08-17 16:56:31 -0700

    Final footer with images

--- services/web-frontend/internal/src/ui/containers/Footer.tsx
+++ services/web-frontend/internal/src/ui/containers/Footer.tsx
@@ -4,15 +4,26 @@ export class Footer extends React.Component {
   render() {
     return (
       <div className="footer-container">
-        <div className="footer-links">
-          <div className="footer-link">
-            <a href="">Link 1</a>
+        <div className="footer-content">
+          <div className="footer-text">
+            Grouphouse v0.0.1
           </div>
-          <div className="footer-link">
-            <a href="">Link 2</a>
-          </div>
-          <div className="footer-link">
-            <a href="">Link 3</a>
+          <div className="footer-links">
+            <div className="footer-link">
+              <a href="">
+                <img src="/images/instagram_logo.png" alt="Instagram Logo" height="20px" width="20px"></img>
+              </a>
+            </div>
+            <div className="footer-link">
+              <a href="">
+                <img src="/images/facebook_logo.png" alt="Facebook Logo" height="20px" width="20px"></img>
+              </a>
+            </div>
+            <div className="footer-link">
+              <a href="">
+                <img src="/images/twitter_logo.png" alt="Twitter Logo" height="20px" width="20px"></img>
+              </a>
+            </div>
           </div>
         </div>
       </div>
